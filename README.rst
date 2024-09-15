b9beedcd199fafd25c85d70c0e636b1b1482fdbf
ntut-xuan <t109590031@ntut.org.tw>
2024-09-16 07:33:00 +0800

    Chore: Add CLI feature

--- README.rst
+++ README.rst
@@ -10,3 +10,10 @@ PolygonDust is an application to measure the area size of polygon.
 polygon size by these pieces.
 
 .. image:: https://media.discordapp.net/attachments/950048467294760990/1284862080050135071/image.png?ex=66e82c8f&is=66e6db0f&hm=8f52c071f46be06d7d6d014e8551087f495e2cf1332a784079c4805429e7aea3&=&format=webp&quality=lossless&width=2880&height=848
+
+Usage
+-----------
+
+```
+python3 ./src/main.py
+```
---
+++ src/main.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from argparse import ArgumentParser, Namespace
+
+
+def initialize_argument_parser() -> ArgumentParser:
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument("-i", "--input", help="Input image.")
+    parser.add_argument("-p", "--particles", help="Particles size (in pixel).")
+    return parser
+
+
+def main():
+    parser: ArgumentParser = initialize_argument_parser()
+    args: Namespace = parser.parse_args()
+
+    if args.input is None:
+        print("Error: You need to input image file.")
+        parser.print_help()
+        return
+
+    if args.particles is None:
+        print("Error: You need to input particles size.")
+        parser.print_help()
+        return
+
+    print("Input image:", args.input)
+    print("Particles size:", args.particles, "px")
+
+
+if __name__ == "__main__":
+    main()
