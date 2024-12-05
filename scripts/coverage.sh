28203e9e07d9fad0a5f964f736f9dc6f81aaf5d2
ntut-xuan <t109590031@ntut.org.tw>
2024-12-05 21:58:29 +0800

    Fix: Coverage report

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -23,7 +23,7 @@ cmake --build $BUILD_DIR
 "./${BUILD_DIR}/build/PolygonDust-gtest"
 
 if [ "$1" = 'xml' ]; then
-    gcovr . --exclude "_deps"  --exclude "test"  --exclude "pyb_includes" --exclude "coverage_build/_deps" --exclude "build/_deps" --exclude "src/main_pyb.cpp" -b --exclude-unreachable-branches --xml -o coverage.xml
+    gcovr . --exclude "_deps"  --exclude "test"  --exclude "pyb_includes" --exclude "coverage_build/_deps" --exclude "build/_deps" --exclude "src/main_pyb.cpp" -b --exclude-unreachable-branches --xml -o ./${BUILD_DIR}/coverage.xml
 fi
 
 if [ "$1" = "html" ]; then
