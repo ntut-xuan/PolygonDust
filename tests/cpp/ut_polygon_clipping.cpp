3044a2b536353a013568bf2b1346425098c0af29
ntut-xuan <t109590031@ntut.org.tw>
2024-12-23 01:12:36 +0800

    Add: Tests

--- tests/cpp/ut_polygon_clipping.cpp
+++ tests/cpp/ut_polygon_clipping.cpp
@@ -16,3 +16,20 @@ TEST(POLYGON_CLIPPING_TEST, test_create_vertex_list_should_return_correct_vertex
     ASSERT_EQ(points, std::vector<Point>({Point(1, 5), Point(3, 2), Point(6, 6), Point(8, 5), Point(10, 4),
                                           Point(11.5, 10), Point(12, 12), Point(6, 12)}));
 }
+
+TEST(POLYGON_CLIPPING_TEST,
+     test_create_vertex_list_with_multiple_intersect_in_the_same_line_should_return_correct_vertex_list) {
+    Polygon polygon1({Point(1, 5), Point(1, 1), Point(4, 1), Point(4, 5)});
+    Polygon polygon2(
+        {Point(5, 4), Point(3, 4), Point(3, 6), Point(2, 6), Point(2, 0), Point(3, 0), Point(3, 2), Point(5, 2)});
+    std::shared_ptr<Polygon> polygon_ptr1 = std::make_shared<Polygon>(polygon1);
+    std::shared_ptr<Polygon> polygon_ptr2 = std::make_shared<Polygon>(polygon2);
+    PolygonClipping polygon_clipping(polygon_ptr1, polygon_ptr2);
+    std::vector<Point> points;
+
+    polygon_clipping.CreateVertexList(points, polygon_ptr2, polygon_ptr1);
+
+    ASSERT_EQ(points, std::vector<Point>({Point(5, 4), Point(4, 4), Point(3, 4), Point(3, 5), Point(3, 6), Point(2, 6),
+                                          Point(2, 5), Point(2, 1), Point(2, 0), Point(3, 0), Point(3, 1), Point(3, 2),
+                                          Point(4, 2), Point(5, 2)}));
+}
